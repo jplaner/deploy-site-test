687142636c0154ddf10bca97fff5e1392811900f
Joseph Planer <josephplaner@Josephs-MacBook-Pro.local>
2023-03-12 12:32:16 -0400

    setup for tutorial

---
+++ src/firebase-config.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBJcxXHy6sv8R5hIo9mcMegJf5MyrEHbCU",
+    authDomain: "test-deploy-1f979.firebaseapp.com",
+    projectId: "test-deploy-1f979",
+    storageBucket: "test-deploy-1f979.appspot.com",
+    messagingSenderId: "830061008775",
+    appId: "1:830061008775:web:63b2a698fd8e45a8062522"
+  };
+
+  const app initializeApp(firebaseConfig);
