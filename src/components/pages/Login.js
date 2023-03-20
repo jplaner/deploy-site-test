d04e3dc19199cf08e2c5e2939de910e31489cf3b
Joseph Planer <josephplaner@Josephs-MacBook-Pro.local>
2023-03-19 21:38:42 -0400

    Firebase authentication setup

--- src/components/pages/Login.js
+++ src/components/pages/Login.js
@@ -1,9 +1,42 @@
-import React from 'react';
+import { signInWithEmailAndPassword } from '@firebase/auth'
+import React, { useState } from 'react';
+import { auth } from '../../firebase-config'
+import AuthDetails from './AuthDetails';
+
 const Login = () =>{
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const signIn = (e) => {
+        e.preventDefault();
+        signInWithEmailAndPassword(auth, email, password).then((userCredential) => {
+            console.log(userCredential);
+        }).catch((error) => {
+            console.log(error);
+        });
+    }
+
     return(
-    <div>
-        <h1>Login</h1>
-    </div>
+        <div className='sign-in-container'>
+            <form onSubmit = {signIn}>
+                <h1>Log In to your Account</h1>
+                <input 
+                    type="email" 
+                    placeholder="Enter your email" 
+                        value={email} 
+                        onChange={(e) => setEmail(e.target.value)}>
+                </input>
+                <input 
+                    type="password" 
+                    placeholder="Enter your password" 
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}>
+                </input>
+                <button type="submit">Log In</button>
+            </form>
+            <AuthDetails />
+        </div>
+        
     );
 };
 export default Login;
